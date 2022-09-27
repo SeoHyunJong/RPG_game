b13664a3c02988dbf5744ad975d36f7a63ddf88f
SeoHyunJong <shj971219@gmail.com>
2022-09-27 22:11:10 +0900

    world quest 띄우기

--- vr_personal_proj/Assets/Script/EnergyBolt.cs
+++ vr_personal_proj/Assets/Script/EnergyBolt.cs
@@ -16,7 +16,7 @@ public class EnergyBolt : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (other.tag != "Player") {
+        if (other.tag != "Player" && other.tag != "NPC") {
             Instantiate(explosion, transform.position, transform.rotation);
             Destroy(gameObject);
         }
